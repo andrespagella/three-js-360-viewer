14475b2bca01b07d6097e1e847c0b2667cb7359a
Andres Pagella <andres.pagella@gmail.com>
2025-03-03 19:39:47 -0300

    arregle un bug con el video de la AI en iOS

--- src/components/ViewerHeader.jsx
+++ src/components/ViewerHeader.jsx
@@ -8,7 +8,7 @@ const ViewerHeader = () => {
     <div 
       className="absolute top-0 left-0 right-0 flex items-center justify-between p-2 z-50 shadow-sm"
       style={{ 
-        backgroundColor: theme.background.primary,
+        backgroundColor: '#FFF',
         borderBottom: `1px solid ${theme.border.light}`,
         color: theme.text.primary
       }}
@@ -25,6 +25,8 @@ const ViewerHeader = () => {
             autoPlay 
             loop 
             muted
+            playsInline
+            webkit-playsinline="true"
           />
         </div>
       </div>
